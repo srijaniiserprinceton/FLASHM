5ffddf051265fcd9042ad5f4d9b83572e900eb76
Lucas Sawade <lsawade@princeton.edu>
2019-04-24 21:40:23 -0400

    first two schemes work 3 to go

--- src/run.py
+++ src/run.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 def main():
     N_cells = 200       # number of cells
     N = N_cells + 1     # N+1 is the number of cell edges.
-    CFL = 0.1           # CFL number
+    CFL = 0.5           # CFL number
     dx = 1.0 / N_cells  # grid spacing
     sig = 0.05          # sigma for the gaussian in the initial function
     v = 1.0             # advection velocity
-    T = 0.5/v           # Length of domain in code units is 1.0
+    T = 1/v           # Length of domain in code units is 1.0
     alpha = 4.0         # parameter for defining the MC limited
 
     # Setting Configuration.
@@ -19,26 +19,28 @@ def main():
 
 
     # Initalize the solver
-    bc = "fixed"
+    bc = "periodic"
     time_stepping = "SSPRK3"
     reconstruction_method = "first_order_upwind"
+    reconstruction_method = "second_order_centered"
 
     flashm = FLASHM(config, bc=bc, method=reconstruction_method,
                  time_ep_method=time_stepping, T=T)
 
+    # plot
+    plt.ion()
+    plt.figure()
     t = 0
     while t<T:
         # Run the solver
         phi_new = flashm.one_time_step()
 
-        # plot
-        plt.ion()
-        plt.figure()
+        plt.title(str(t))
         plt.plot(config.x[1:], flashm.init_avg(), label="Initial profile")
         plt.plot(config.x[1:], phi_new, label="Profile after time T")
         # plt.plot(config.x[1:], phi_new-flashm.init_avg(), label="Profile after time T")
         plt.legend()
-        plt.pause(0.1)
+        plt.pause(0.001)
         plt.clf()
 
         t += config.dt
