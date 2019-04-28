bb2841ec34a4421d84bc46f2169ab1a2e965f69d
Lucas Sawade <lsawade@princeton.edu>
2019-04-27 23:43:22 -0400

    must commit before revert

--- src/run.py
+++ src/run.py
@@ -1,32 +1,17 @@
 from flashm import Config, FLASHM
 
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def velocity_profile(x):
-    """
-    :param x:
-    :return: velocity profile
-    """
-    # sine-wave
-    # return np.sin(2*np.pi*x)
-    # return np.cos(2*np.pi*x)
-    return np.ones(len(x))*2
-    # return np.sqrt(x)+1
-    # return 0.5*(x)**3
-
 def main():
-    N_cells = 200      # number of cells
+    N_cells = 200       # number of cells
     N = N_cells + 1     # N+1 is the number of cell edges.
     CFL = 0.25           # CFL number
     dx = 1.0 / N_cells  # grid spacing
     sig = 0.05          # sigma for the gaussian in the initial function
     v = -1.0             # advection velocity
     T = 1/np.abs(v)           # Length of domain in code units is 1.0
-
-
+    alpha = 4.0         # parameter for defining the MC limited
 
     # Setting Configuration.
     config = Config(dim=1, cells=N_cells, CFL=CFL, dx=dx, sigma=sig, v=v,
@@ -34,7 +19,7 @@ def main():
 
 
     # Initalize the solver
-    bc = "periodic"
+    bc = "outgoing"
 
     time_stepping = "SSPRK3"
 
@@ -50,9 +35,11 @@ def main():
     # plot
     plt.ion()
     plt.figure()
+    plt.ylim([-.5, 1.5])
+    plt.xlim([0, 1])
     t = 0
 
-<<<<<<< HEAD
+
     # Start countin'
     start = time.time()
 
@@ -65,8 +52,8 @@ def main():
 
     while t<T-config.dt:
         # Run the solver
-
         phi_new = flashm.one_time_step()
+
         counter += 1
         energy[counter] = np.linalg.norm(phi_new)/rel_energy
         if counter%10:
@@ -80,38 +67,19 @@ def main():
             plt.legend(loc=2)
             plt.pause(0.001)
             plt.clf()
-=======
-    while t<T:
-        # Run the solver
-        plt.ylim([-.5, 1.5])
-        plt.xlim([0, 1])
-        phi_new = flashm.one_time_step()
-
-        plt.title("%2.3f s" % t)
-        plt.plot(config.x[1:], flashm.init_avg(), label="Initial profile")
-        plt.plot(config.x[1:], phi_new, label="Profile after time T")
-        # plt.plot(config.x[1:], phi_new-flashm.init_avg(), label="Profile after time T")
-        plt.legend(loc=2)
-        plt.pause(0.001)
-        plt.clf()
->>>>>>> parent of 087aec4... EVERYTHING WORKS!!!!!!
 
         t += config.dt
 
-    # End counting the time
-    end = time.time()
 
-    # Print time
-    print("Time expired: %4.1f seconds." % (end - start))
-
+    # Run the solver
+    plt.ylim([-.5, 1.5])
+    plt.xlim([0, 1])
 
 
     plt.title("%2.3f s" % t)
-    plt.ylim([-.5, 1.5])
-    plt.xlim([0, 1])
     plt.plot(config.x[1:], flashm.init_avg(), label="Initial profile")
     plt.plot(config.x[1:], phi_new, label="Profile after time T")
-    plt.plot(t1[:counter + 1], energy[:counter + 1], label="Energy")
+    # plt.plot(config.x[1:], phi_new-flashm.init_avg(), label="Profile after time T")
     plt.legend(loc=2)
     plt.show(block=True)
 
