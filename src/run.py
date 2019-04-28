9fe39c39e0d5760c6c9e71dddbff6333f8007ccb
Lucas Sawade <lsawade@princeton.edu>
2019-04-27 23:54:30 -0400

    Everything is working again... except for strange velocity models..

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
     N_cells = 200       # number of cells
     N = N_cells + 1     # N+1 is the number of cell edges.
-    CFL = 0.25           # CFL number
+    CFL = 0.5           # CFL number
     dx = 1.0 / N_cells  # grid spacing
     sig = 0.05          # sigma for the gaussian in the initial function
     v =  1.0             # advection velocity
@@ -39,20 +39,27 @@ def main():
     plt.xlim([0, 1])
     t = 0
 
+    counter = 0
+    t1 = np.arange(0, T+config.dt, config.dt)
+    energy = np.zeros(len(t1))
+    energy[0] = np.linalg.norm(flashm.phi)
+    rel_energy = energy[0]
+    energy[0] = 1
+
     while t<T-config.dt:
         # Run the solver
         phi_new = flashm.one_time_step()
 
         counter += 1
         energy[counter] = np.linalg.norm(phi_new)/rel_energy
-        if counter%10:
+
+        if counter%100:
             plt.ylim([-.5, 1.5])
             plt.xlim([0, 1])
             plt.title("%2.3f s" % t)
             plt.plot(config.x[1:], flashm.init_avg(), label="Initial profile")
             plt.plot(config.x[1:], phi_new, label="Profile after time T")
             plt.plot(t1[:counter+1], energy[:counter+1], label="Energy")
-            # plt.plot(config.x[1:], phi_new-flashm.init_avg(), label="Profile after time T")
             plt.legend(loc=2)
             plt.pause(0.001)
             plt.clf()
